15da4faeeb6442ba67999abd221145f7966b94e7
Brenopms <brenosilva1@gmail.com>
2025-04-29 10:10:34 -0300

    fix: update barrel files

--- src/lib/combinators/logic/index.ts
+++ src/lib/combinators/logic/index.ts
@@ -1,4 +1,6 @@
 export * from './all-rules';
+export * from './branch';
 export * from './not';
 export * from './one-of';
+export * from './unless';
 export * from './when';
--- src/lib/types/index.ts
+++ src/lib/types/index.ts
@@ -1,3 +1,4 @@
 export * from './composition-options';
+export * from './debug-options';
 export * from './rule-result';
 export * from './rule';
